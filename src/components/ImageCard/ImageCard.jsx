61b77e7ed876868ec018e7515e23ad1ceeb4b544
Maryna Bobryk <maryna@Mac.fritz.box>
2025-04-19 22:02:54 +0200

    fix ImageGallery

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,7 +4,6 @@ import ImageModal from '../ImageModal/ImageModal';
 const ImageCard = ({ image, openModal }) => {
   return (
     <div>
-      {/* <a href={image.href} target='_blank'></a> */}
       <img className={s.galleryImage} src={image.urls.small} alt={image.alt_description} onClick={() => openModal(image)} />
     </div>
   );
